8de08f79f702c74a4fe3bf1ac7aa91ca0db9b1a7
Rafał Kapuściński <rafal.kapuscinski@snpgroup.com>
2021-04-26 21:07:21 +0200

    add some icons to navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faAddressCard, faHome, faReceipt, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  faHome = faHome;
+  faProducts = faReceipt;
+  faAboutUs = faUsers;
+  faContact = faAddressCard;
 
   constructor() { }
 
